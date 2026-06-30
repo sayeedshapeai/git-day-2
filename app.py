05563469378273243d466dccb29a3553ad46a68b
Sayeed Uddin Shaik <sayeedmohd1704@gmail.com>
2026-06-29 21:57:24 -0400

    Implement basic HTTP server in Python

---
+++ app.py
@@ -0,0 +1,17 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(b"Hello from Python Server!")
+
+HOST = "0.0.0.0"
+PORT = 8000
+
+server = HTTPServer((HOST, PORT), MyHandler)
+
+print(f"Server is running on http://{HOST}:{PORT}")
+
+server.serve_forever()
